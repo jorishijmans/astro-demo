a9421531d33452097d90cf97fdcfcc0eea2b41f2
Joris Hijmans <contact@jorishijmans.com>
2025-06-10 14:34:32 +0000

    Update name
    Signed-off-by: Joris Hijmans <contact@jorishijmans.com>

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Demo';
-export const SITE_DESCRIPTION = 'Welcome to my Astro Demo';
+export const SITE_TITLE = 'JH Astro Demo';
+export const SITE_DESCRIPTION = 'Welcome to JH Astro Demo';
 
-
-// Languages
 
